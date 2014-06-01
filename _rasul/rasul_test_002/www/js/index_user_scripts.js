cb30d2e5fd75addb2e22ee95e3aeeedc5d02482e
Racoo-Bomba <rmuhamend@kheprw.org>
2014-06-01 11:35:15 -0400

    second commit
    progress of new provider and offer pages

---
+++ _rasul/rasul_test_002/www/js/index_user_scripts.js
@@ -0,0 +1,23 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+         
+        
+        
+        
+        
+        
+        $(document).on("click", ".uib_w_77", function(evt)
+        {
+         activate_subpage("#catering_3"); 
+        });
+}
+ $(document).ready(register_event_handlers);
+})();
