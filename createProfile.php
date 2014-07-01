87fe2485f1e469f9aa26d336634f37837cafb861
puishi <puishi@kheprw.org>
2014-07-01 17:47:10 -0400

    Pambana's Fifth Commit
    Revised  Register page and added  Create Profile page to show  provider
    offering after registration and provide option to add offering.

--- createProfile.php
+++ createProfile.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0">
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
 	<link rel="stylesheet" href="themes/Neighborly.min.css">   
-    <title>Provider Detail</title>
+    <title>View Profile</title>
 	<script src="intelxdk.js"></script>
     <script src="cordova.js"></script>
     <script type="application/javascript" src="js/jquery.min.js"></script>
@@ -31,7 +31,7 @@
           
           <div data-role="header" style="overflow:hidden;">
             <a href="#" data-icon="carat-l" class="ui-btn-left" style="background-color:#000000;" data-transition="fade" data-rel="back">Back</a>
-            <h1 style="vertical-align: middle; text-align: center;">Kheprw</h1>
+            <h1 style="vertical-align: middle; text-align: center;">View Profile</h1>
             <a href="#" data-icon="action" class="ui-btn-right" style="background-color:#000000">Sign Out</a>
          </div><!--/header-->
           
@@ -39,15 +39,20 @@
             <div class="grid grid-pad urow uib_row_15 row-height-15" data-uib="layout/row" data-ver="0">
               <div class="col uib_col_29 col-0_12-12" data-uib="layout/col" data-ver="0">
                 <div class="widget-container content-area vertical-col">
+                          
+                <div class="widget-container content-area vertical-col">
+                 	<div style="margin-left:30px; margin-right: 30px;">
+                      <h4>Kheprw</h4>
+            	  	</div>
+                 </div>
 
                   <div class="widget uib_w_70 scale-image d-margins" data-uib="media/img" data-ver="0">
                     <figure class="figure-align" align="left" style="margin-left:30px; margin-top:10px;">
                      <img src="images/ki_trsp.png" width="128" height="128" alt="KI Logo">
                     </figure>
                   </div>
+                  
                   <div class="widget uib_w_73 d-margins phone" data-uib="media/text" data-ver="0">
-                    <div class="widget-container left-receptacle"></div>
-                    <div class="widget-container right-receptacle"></div>
                     <div style="margin-left:30px; margin-right: 30px;">
                       <p> <a href="http://www.kieocenter.org">www.kieocenter.org</a></p>
                       <a href="#" class="ui-btn ui-corner-all" data-mini="true">Contact Us</a>
@@ -71,7 +76,7 @@
                           Indianapolis, IN 46208<br>
                           info@kheprw.org</p>
                     </div>
-                    <div class="widget uib_w_94 d-margins iframe-size" data-uib="media/iframe" data-ver="0" style="margin-left:30px; margin-right:30px;">
+                    <div class="widget uib_w_94 d-margins iframe-size" data-uib="media/iframe" data-ver="0" style="margin-left:30px; margin-right:30px;"><!-- map only shown for organizations -->
                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3064.4096585690654!2d-86.16358089999999!3d39.82023199999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886b514675002ba7%3A0x767a2b5646e17170!2s3549+Boulevard+Pl!5e0!3m2!1sen!2sus!4v1400644897818" height="300" width="100%" frameborder="0" style="border:0" align="left"></iframe>
                   </div>
                 </div>
@@ -84,89 +89,15 @@
               <div class="col uib_col_33 col-0_12-12" data-uib="layout/col" data-ver="0">
                 <div class="widget-container content-area vertical-col">
                  	<div style="height=50px;"data-ver="0">
-                      <p>What We Offer</p>
+                      <p>Options</p>
             	  	</div>
                  </div>
-               
-             
-                  <div data-role="collapsible-set" class="no_wrap widget uib_w_74 d-margins" data-uib="jquery_mobile/accordion" data-ver="0" data-collapsed-icon="plus" data-expanded-icon="minus" data-theme="a">
-                    <div data-role="collapsible" class="no_wrap widget uib_w_75" data-uib="jquery_mobile/collapsible" data-ver="0" data-mini="true" data-collapsed-icon="eye">
-                      <h4>KI NuMedia</h4>
-                      <div class="col uib_col_34 single-col" data-uib="layout/col" data-ver="0">
-                        <div class="widget-container content-area vertical-col">
-
-                          <div class="widget uib_w_79 d-margins link" data-uib="media/text" data-ver="0">
-                            <div class="widget-container left-receptacle"></div>
-                            <div class="widget uib_w_78 scale-image d-margins" data-uib="media/img" data-ver="0">
-                            <figure class="figure-align">
-                              <img src="images/NuMedia Logo.png"  width="100%" alt="Logo" longdesc="images/NuMedia Logo.png">
-<figcaption data-position="bottom"></figcaption>
-                            </figure>
-                          </div>
-
-                          <span class="uib_shim"></span>
-                        </div>
-                        <div>
-                              <p>We offer graphic design, web/mobile app development, social media management and branding services.
-                        </div>
-                          </div>
-                            <div class="widget-container right-receptacle">
-                              <a href="offeringDetail.php" class="widget uib_w_81 d-margins" data-uib="jquery_mobile/button" data-ver="0" data-role="button" data-mini="true">More Info</a>
-                            </div>
-                            
-                      </div>
-                    </div>
-                    <div data-role="collapsible" class="no_wrap widget uib_w_76" data-uib="jquery_mobile/collapsible" data-ver="0" data-mini="true" data-collapsed-icon="eye">
-                      <h4>EY Rainbarrels</h4>
-                      <div class="col uib_col_34 single-col" data-uib="layout/col" data-ver="0">
-                        <div class="widget-container content-area vertical-col">
-
-                          <div class="widget uib_w_79 d-margins link" data-uib="media/text" data-ver="0">
-                            <div class="widget-container left-receptacle"></div>
-                            <div class="widget uib_w_78 scale-image d-margins" data-uib="media/img" data-ver="0">
-                            <figure class="figure-align">
-                              <img src="images/hoam.jpg" height="100px"> 
-                              <figcaption data-position="bottom"></figcaption>
-                            </figure>
-                          </div>
-
-                          <span class="uib_shim"></span>
-                        </div>
-                        <div>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. olor sit am etur adip</p>
-                        </div>
-                          </div>
-                            <div class="widget-container right-receptacle">
-                              <a href="" class="widget uib_w_81 d-margins" data-uib="jquery_mobile/button" data-ver="0" data-role="button" data-mini="true">More Info</a>
-                            </div>
-                            
-                      </div>
-                    </div>
-                    <div data-role="collapsible" class="no_wrap widget uib_w_77" data-uib="jquery_mobile/collapsible" data-ver="0" data-mini="true" data-collapsed-icon="eye">
-                      <h4>EcoFashion Bags</h4>
-                      <div class="col uib_col_34 single-col" data-uib="layout/col" data-ver="0">
-                        <div class="widget-container content-area vertical-col">
-
-                          <div class="widget uib_w_79 d-margins link" data-uib="media/text" data-ver="0">
-                            <div class="widget-container left-receptacle"></div>
-                            <div class="widget uib_w_78 scale-image d-margins" data-uib="media/img" data-ver="0">
-                            <figure class="figure-align">
-                              <img src="images/street.png" height="100px"> 
-                              <figcaption data-position="bottom"></figcaption>
-                            </figure>
-                          </div>
-
-                          <span class="uib_shim"></span>
-                        </div>
-                        <div>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. olor sit am etur adip</p>
-                        </div>
-                          </div>
-                            <div class="widget-container right-receptacle">
-                              <a href="" class="widget uib_w_81 d-margins" data-uib="jquery_mobile/button" data-ver="0" data-role="button" data-mini="true">More Info</a>
-                            </div>
-                      </div>
-                </div>
+                 <div class="widget-container content-area vertical-col">
+                 <a href="#addOffering.html" class="ui-btn ui-corner-all">Add an Offering</a>
+                 <a href="#" class="ui-btn ui-corner-all">Update Provider Info</a>
+                 <a href="index.html" class="ui-btn ui-corner-all">Search Providers</a>
+                 </div>
+                  
               
           <div data-role="footer" style="overflow:hidden;" data-position="fixed">
 			<div data-role="navbar">
